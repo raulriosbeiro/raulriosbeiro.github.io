619065141d60fea0488611994628c8550e9bcd9e
raulriosbeiro <111523720+raulriosbeiro@users.noreply.github.com>
2022-10-20 20:57:42 +0200

    ghj

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ function onScanSuccess(decodedText, decodedResult) {
 
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
- "qr-reader", { fps: 30, qrbox: 250 });
+ "qr-reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess);
